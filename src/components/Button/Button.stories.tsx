e3f193fb4821bd01a753665dbc89a6bf96932085
AlperYoruk <alper.yoruk@arabam.com>
2021-09-11 01:35:40 +0300

    init

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import { Story } from "@storybook/react";
+import Button, { ButtonProps } from "./Button";
+
+export default {
+    title: "Components/Button",
+    component: Button,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+// Reuse that template for creating different stories
+export const Primary = Template.bind({});
+Primary.args = { label: "Primary 😃", size: "large" };
+
+export const Secondary = Template.bind({});
+Secondary.args = { ...Primary.args, primary: false, label: "Secondary 😇" }
